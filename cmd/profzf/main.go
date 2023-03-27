6d094d026cb529be736c100d7d9d9b3a842202dc
Emil Edholm <bobby@verypublic.org>
2023-03-27 20:28:13 +0200

    fix(cmd): graceful shutdown on sigterm
    I.e. when you do kill

--- cmd/profzf/main.go
+++ cmd/profzf/main.go
@@ -5,10 +5,11 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"syscall"
 )
 
 func main() {
-	ctx, cf := signal.NotifyContext(context.Background(), os.Interrupt)
+	ctx, cf := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer cf()
 	if cmd, err := newRootCommand().ExecuteContextC(ctx); err != nil {
 		log.Printf("failed to execute %s: %v", cmd.Name(), err)
